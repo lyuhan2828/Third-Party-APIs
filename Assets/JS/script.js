3367783e76fba16b9e1d6e51ef9eebc32100bf92
Yu Han <yuhan28283@gmail.com>
2022-03-24 19:16:22 -0400

    updated calender

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -15,22 +15,28 @@ $(document).ready(()=>{
     $("#16 .description").text(localStorage.getItem("16"))
     $("#17 .description").text(localStorage.getItem("17"))
 
+
+
     function checkHour(){
         var currentHour = moment().hours()
+        console.log(currentHour)
         $(".time-block").each(function() {
+            console.log($(this))
             var blockHour = parseInt($(this).attr("id"))
             if (currentHour === blockHour) {
-                $(this).addClass("present")
+                $(this).children().addClass("present")
             }
-            else if (currentHour < blockHour) {
-                $(this).removeClass("past")
+            else if (currentHour > blockHour) {
+                $(this).children().addClass("past")
             }
-            else (currentHour > blockHour) ;{
-                $(this).removeClass("future")
+            else {
+                $(this).children().addClass("future")
             }
         })
     }
+    
+    checkHour();
 
-    $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+    $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:a'))
 
 })
